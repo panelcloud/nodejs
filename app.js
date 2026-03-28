8131816dfbb4a523f6b3e8ba0a279ce45e83b2f6
panelcloud <143173339+panelcloud@users.noreply.github.com>
2026-03-28 13:53:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas
+app.use('/auth', require('./routes/auth'));
 app.use('/', require('./routes/index'));
 
 app.use((req, res) => {
